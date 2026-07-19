71e009dff80c33172742e46d8488ad8bb3aac2b2
varsha kumari <varshakumari2714@gmail.com>
2026-07-19 22:05:30 +0530

    add Delete api

--- src/app.js
+++ src/app.js
@@ -20,5 +20,11 @@ app.get("/api/notes", async (req, res) => {
     noteData,
   });
 });
- 
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "Delete Post Successfully",
+  });
+});
 module.exports = app;
